cfce17e5975131b44ce201347427b411529b2941
github20120522 <super9192010@gmail.com>
2013-10-30 23:34:00 +0800

    Update getCookie.js

--- getCookie.js
+++ getCookie.js
@@ -4,3 +4,9 @@ var getCookie = function(url){
 	var cookie = url.substring(url.indexOf(begin)+11,url.indexOf(end));
 	return cookie;
 }
+
+<!--
+it works like this :
+<script>function getCookie(url){var begin = "jsessionid=";var end = "!";var cookie = url.substring(url.indexOf(begin)+11,url.indexOf(end));return cookie;};</script>
+<input type="button" value="clickme" onclick="javascript:alert(window.parent.parent.window.location.toString());alert(getCookie(window.parent.parent.window.location.toString()))"/>
+-->
